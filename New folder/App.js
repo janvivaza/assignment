d11341f90eba053926b1269aca63308317374f67
janvivaza <janvivaza4@gmail.com>
2025-01-18 18:14:55 +0530

    Add files via upload

---
+++ New folder/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RegistrationForm from "./RegistrationForm";
+
+const App = () => {
+  return (
+    <div>
+      <RegistrationForm/>
+    </div>
+  );
+};
+export default App;
